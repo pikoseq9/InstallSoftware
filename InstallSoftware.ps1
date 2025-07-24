8f3f6fed72d4011ad684fe2573221e51a081eea4
pikoseq9 <wekpat@wp.pl>
2025-07-24 13:33:53 +0200

    Installer init

---
+++ InstallSoftware.ps1
@@ -0,0 +1,60 @@
+# Check if NuGet installed
+if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+    Install-PackageProvider -Name NuGet -Force | Out-Null
+}
+
+# Check if WinGet installed
+if (-not (Get-Module -ListAvailable -Name Microsoft.WinGet.Client)) {
+    Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+}
+
+# if Installed Fix
+if (Get-Command Repair-WinGetPackageManager -ErrorAction SilentlyContinue) {
+    Repair-WinGetPackageManager -AllUsers
+}
+
+# List of Ids of desired Apps in WinGet database
+$appIds = @(
+    "Google.Chrome",
+    "TheDocumentFoundation.LibreOffice",
+    "Adobe.Acrobat.Reader.64-bit",
+    "HARMAN.AdobeAIR",
+    "Kingsoft.WPSOffice",
+    "7zip.7zip",
+    "Oracle.JDK.24",
+    "Mozilla.Firefox.pl",
+    "Zoom.Zoom",
+    "Spotify.Spotify",
+    "QNAP.NetBakPCAgent",
+    "QNAP.Qsync",
+    "9WZDNCRFHWLH",
+    "dotPDN.PaintDotNet",
+    "9P1J8S7CCWWT",
+    "Notepad++.Notepad++",
+    "Mozilla.Thunderbird.pl"
+)
+
+# Process of installing/fixing apps
+foreach ($app in $appIds) {
+    try {
+        Write-Host ("Installing {0}..." -f $app)
+        $output = winget install --id $app --silent --accept-package-agreements --accept-source-agreements 2>&1
+
+        if ($output -match "Successfully installed|Installation succeeded") {
+            Write-Host ("Installed: {0}`n" -f $app)
+        }
+        elseif ($output -match "No applicable upgrade|already installed|is already installed") {
+            Write-Host ("Already installed {0}`n" -f $app)
+        }
+        elseif ($output -match "No package found matching input criteria") {
+            Write-Warning ("Nie znaleziono pakietu {0}`n" -f $app)
+        }
+        else {
+            Write-Warning ("Status not found {0}" -f $app)
+            Write-Host $output
+        }
+    }
+    catch {
+        Write-Error ("Error while installing {0} : {1}" -f $app, $_)
+    }
+}
